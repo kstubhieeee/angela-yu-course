f958eb51daeac8be1e869493aa291ac0841d97a0
kstubhie <banekaustubh27@gmail.com>
2024-06-23 01:18:49 +0530

    step 6

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -1,7 +1,34 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColor);
+}
+
+$(".btn").on("click", function (event) {
+  var userChosenColor = event.target.id;
+  userClickedPattern.push(userChosenColor);
+  animatePress(userChosenColor);
+  playSound(userChosenColor);
+});
+
+function playSound(name) {
+  var audio = new Audio("./sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("." + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("." + currentColor).removeClass("pressed");
+  }, 100);
 }
