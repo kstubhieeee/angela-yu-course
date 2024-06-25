f3ba769b42daf67bd74b4539e2016b82d3742031
kstubhie <banekaustubh27@gmail.com>
2024-06-26 01:03:31 +0530

    node

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
-fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
+fs.writeFile("message1.txt", "Hello Kaustubh", (err) => {
   if (err) throw err;
   console.log("The file has been saved!");
+});
+
+fs.readFile("./message1.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
 });
