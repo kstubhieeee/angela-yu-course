4651bd5eb416c5639766a73dd08e17fab7c7505b
kstubhie <banekaustubh27@gmail.com>
2024-06-26 15:00:26 +0530

    npm superheroes practice

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -1,3 +1,3 @@
-import generateName from "sillyname";
-var sillyname = generateName();
-console.log("My name is " + sillyname);
+import { randomSuperhero } from "superheroes";
+var name = randomSuperhero();
+console.log("I'm " + name);
