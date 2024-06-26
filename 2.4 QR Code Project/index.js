ed28a82f2f95edf69b00e6e245f4dcf81d0a2554
kstubhie <banekaustubh27@gmail.com>
2024-06-26 18:24:19 +0530

    qr-code

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -4,6 +4,25 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 
-import { input } from "inquirer";
-var user = input();
-console.log(user);
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+inquirer
+  .prompt([
+    {
+      message: "Type your URL : ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
