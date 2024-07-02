84147fc4179dbde2316e56716780f0154af30e4e
kstubhie <banekaustubh27@gmail.com>
2024-07-02 13:20:27 +0530

    middleware

--- 3.4 Middleware/index3.js
+++ 3.4 Middleware/index3.js
@@ -3,9 +3,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next) {
+  console.log("Enter method: ", req.method);
+  console.log("Enter URL", req.url);
+  next();
+}
+
 app.use(logger);
 
-app.get("/", (req, res) => {
+app.get("/about", (req, res) => {
   res.send("Hello");
 });
 
