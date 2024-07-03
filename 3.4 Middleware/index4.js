61d96e7ca6e1710bfce1839a776e9fc4b4827b89
kstubhie <banekaustubh27@gmail.com>
2024-07-03 11:15:20 +0530

    middleware

--- 3.4 Middleware/index4.js
+++ 3.4 Middleware/index4.js
@@ -1,8 +1,35 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+var bandName = "";
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function bandNameGenerator(req, res, next) {
+  bandName = req.body["street"] + req.body["pet"];
+  next();
+}
+
+app.use(bandNameGenerator);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>${bandName}🦍</h1> <style>body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}</style>`);
+  console.log(req.body);
 });
