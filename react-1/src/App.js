14b7fa8e29e58d99680b49bb035e3c7bafef1413
kstubhie <banekaustubh27@gmail.com>
2024-08-06 00:08:00 +0530

    react

---
+++ react-1/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>I love my pookie so much</h1>
+      <h2>Radhika is gorgeous</h2>
+    </div>
+  );
+}
---
+++ react-1/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favourite foods</h1>
+    <ul>
+      <li>Papad</li>
+      <li>Pav Bhaji</li>
+      <li>Mango</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
