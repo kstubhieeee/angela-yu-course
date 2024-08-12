6e0b129c15d58d7095e593d94d83b7c2127f7a9f
Kaustubh <144565193+kstubhieeee@users.noreply.github.com>
2024-08-12 20:17:08 +0530

    Update App.js

--- react-1/src/App.js
+++ react-1/src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>I love my pookie so much</h1>
-      <h2>Radhika is gorgeous</h2>
+      <h1>I love my ...... so much</h1>
+      <h2>....... is gorgeous</h2>
     </div>
   );
 }
