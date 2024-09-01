e345c17d37a40a899192f2db270d140c7a550f47
kstubhie <banekaustubh27@gmail.com>
2024-09-01 17:39:29 +0530

    react components

---
+++ react-components/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import Lists from "./Lists";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Lists />
+      <Lists />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-components/src/components/Lists.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Lists() {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+      <li>Papad</li>
+    </ul>
+  );
+}
+
+export default Lists;
